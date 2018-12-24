67636e0e26a5ff43eadac573c5b1f30472cd4108
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2018-12-25 02:20:20 +0300

    Add files via upload

---
+++ node005_FunctionExpressions.js
@@ -0,0 +1,23 @@
+ // nomal fucntion statment
+
+ function sayHi(){
+   console.log('hi');
+ }
+ sayHi();
+
+ // function  expression
+ var sayBye= function(){   // the thing is this function store in varialbe
+   console.log('bye');     // we can pass this fuction as argument
+ }
+ sayBye();
+
+ // callback function
+
+ function callFunction(fun){
+   fun();
+ };
+
+ var sayBye =function(){
+   console.log('bye');
+ };
+callFunction(sayBye); // we pass the sayBye function as argument
