962683d402df78720a4258a3d45bed24a538e514
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2018-12-25 02:19:19 +0300

    Add files via upload

---
+++ node006_module.js
@@ -0,0 +1,8 @@
+
+var counter=function(arr){
+
+  return 'There are '+arr.length+' elements is this array';
+}
+
+module.exports=counter;
+// export the 'counter' function to another module from this module
