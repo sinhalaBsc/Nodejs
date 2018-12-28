bf0226aa111c80de6001281576c41f3e8d28e8e0
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2018-12-28 04:40:01 +0300

    Add files via upload

---
+++ node022_InstallingNodemon/app.js
@@ -0,0 +1,16 @@
+// let's talk about one other pachage call 'Nodemon'
+
+/*
+ this is gread package for when you developing
+
+what is this for :
+  when we did some changes on our program it need to restart the server to add
+  new change appear on browser.this package automatically restart our server when
+  we did some change on our program.
+
+installation:
+ $ npm install -g nodemon
+ // for me on linux it's not worked. "Error: EACCES: permission denied, access '/usr/lib/node_modules'", 
+
+
+*/
