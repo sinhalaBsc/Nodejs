7e5492af2eb32aa9fe1df29655eb2fe0cd1998ab
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2018-12-28 04:47:22 +0300

    Add files via upload

---
+++ node020_PackageManager/app.js
@@ -0,0 +1,24 @@
+// refer to lesson 19
+
+/*
+we are tolk when we have more pages and links on the website it is difficult to
+ Routing. now we are going to use some package for that.
+
+ let's tolk about 'node package manager'(npm)
+   * 'npm' is default installing when you are installing nodejs.
+   * 'npm' is bunch of command line tools which help to install thirdpaty packages or moduls.
+   * Express in one of these packages and there are many more.
+   * Express package helps us with Routing and templating and such.
+
+ to get Express package
+   1. go to www.npmjs.com and search 'express' and look info.
+   2. and install package as there syses
+        $ npm install express
+   3. it will make 'node_modules' folder on current directory to all dependency with Express package.
+   4. but we don't wont the other dependency packages. to unstall Express
+       $ npm uninstall express
+   5. it will leave only 'bin' inside 'node_modules' folder. but for me it's install only express file.
+
+
+
+*/
