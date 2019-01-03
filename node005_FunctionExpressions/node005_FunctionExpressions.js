86cbb1976e42a7c09e12a85d67ca127c95cbad20
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2019-01-03 16:49:07 +0300

    Update node005_FunctionExpressions.js

--- node005_FunctionExpressions/node005_FunctionExpressions.js
+++ node005_FunctionExpressions/node005_FunctionExpressions.js
@@ -1,23 +1,25 @@
- // nomal fucntion statment
-
+ // nomal fucntion statment ************************************************
  function sayHi(){
    console.log('hi');
  }
  sayHi();
 
- // function  expression
+
+ // function  expression ***************************************************
  var sayBye= function(){   // the thing is this function store in varialbe
    console.log('bye');     // we can pass this fuction as argument
  }
  sayBye();
 
- // callback function
 
- function callFunction(fun){
-   fun();
+ // callback function ******************************************************
+ function callFunction(fun){   // 'fun' is fuction varible.
+   fun();                      // 'fuc' variable value is 'sayBye' and it is a function.
  };
 
- var sayBye =function(){
+ var sayBye =function(){      // this is anonymous function load on 'sayBye' variable.
    console.log('bye');
  };
-callFunction(sayBye); // we pass the sayBye function as argument
+callFunction(sayBye);         // we pass the sayBye function as argument
+
+
